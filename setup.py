d36cb701c48eef1c090b1fd21457c1a434c09e64
SergDell <zaswed76@gmail.com>
2020-03-07 21:58:24 +0200

    start

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
                 join(dirname(__file__), 'README.rst')).read(),
         install_requires=["PyQt5", "pyaml"],
         entry_points={
-            'gui_scripts':
+            'scripts':
                 ['cube = cube.run:main']
         }
 
